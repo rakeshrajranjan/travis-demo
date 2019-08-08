53bb00ea2c1addcd0ae02403a15d24c99c4867ec
rranjan14 <rranjan14@sapient.com>
2019-08-08 11:54:48 +0530

    2nd commit

---
+++ js/error.test.js
@@ -0,0 +1,5 @@
+import { abc } from "./main";
+
+test("app start", ()=> {
+    expect(abc()).toBe("abc text");
+});
---
+++ js/main.js
@@ -0,0 +1,8 @@
+var abc = function(){
+    var d = "abc text";
+    return d;
+};
+
+console.log(abc());
+
+export {abc}
